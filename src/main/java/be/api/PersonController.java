2f12e011f9cabd2e780b5d75eae55bd942ed7657
Willems Frédéric <f.willems@student.helmo.be>
2020-09-28 11:53:24 +0200

    Fixing endpoint (person -> persons)

--- src/main/java/be/api/PersonController.java
+++ src/main/java/be/api/PersonController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/person")
+@RequestMapping("/api/v1/persons")
 @RestController
 public class PersonController {
 
